507dc0b75150ca547a0d237825a119761e63886d
insidejob <84038103+insidescode@users.noreply.github.com>
2021-07-30 19:04:48 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -52,7 +52,7 @@ from tg_bot.modules.language import gs
 
 PM_START_TEXT = """
 Hi {}, my name is {}!
-I am an Anime themed group management bot with some fun extras [;)](https://telegra.ph/file/095d7e696096e21b06447.jpg)
+I am group management bot with some cool features [;)](https://telegra.ph/file/d70aa70ffd56332f70e2b.jpg)
 
 You can find the list of available commands with /help.
 """
@@ -60,7 +60,7 @@ You can find the list of available commands with /help.
 HELP_STRINGS = """
 Hey there! My name is *{}*.
 I'll be at your service take a look below to know how to use me.
-If you have any questions on how to use me, head over to @YuiiChanSupport
+If you have any questions on how to use me, head over to @Zer0ByteSupport
 *Main* commands available:
  • /start: start the bot
  • /help: PM's you this message.
@@ -77,7 +77,7 @@ And the following:
 )
 
 
-Yuii_IMG = "https://telegra.ph/file/145c818a6b4e5bc92765d.jpg"
+Yuii_IMG = "https://telegra.ph/file/d70aa70ffd56332f70e2b.jpg"
 
 
 IMPORTED = {}
@@ -227,7 +227,7 @@ def start(update: Update, context: CallbackContext):
                         [
                             InlineKeyboardButton(
                                 text=gs(chat.id, "src_btn"),
-                                url="https://github.com/rshero/YuiiChan",
+                                url="https://telegra.ph/file/41387184db3e8e763132c.jpg",
                             ),
                             InlineKeyboardButton(
                                 text="Try inline mode",
@@ -237,7 +237,7 @@ def start(update: Update, context: CallbackContext):
                         [
                             InlineKeyboardButton(
                                 text=gs(chat.id, "support_chat_link_btn"),
-                                url=f"https://t.me/yuiichansupport",
+                                url=f"https://t.me/Zer0ByteSupport",
                             ),
                         ],
                     ]
